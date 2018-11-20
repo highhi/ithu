07e84b5dfd6a1fc99aa2ac80cc0d6e5e16399393
kazu <kazu8160@gmail.com>
2018-11-21 01:30:03 +0900

    serverのコア部分を分離

--- server/index.js
+++ server/index.js
@@ -1,80 +1,22 @@
 'use strict'
 
 require('dotenv').config()
-const path = require('path')
-const { createServer } = require('http')
-const express = require('express')
-const bodyParser = require('body-parser')
-const helmet = require('helmet')
-const compression = require('compression')
-const session = require('express-session')
-const csurf = require('csurf')
-const logger = require('morgan')
-const router = require('./router')
+const runServer = require('./server')
 
-const port = process.env.PORT || 3000
-const app = express()
-const sessionOptions = {
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    //secure: true,
-    httpOnly: true,
-    domain: process.env.HOST,
-  }
+runServer()
+
+if (process.env.NODE_ENV === 'development') {
+  process.on('unhandledRejection', (err, p) => {
+    console.error('Error : ', err)
+    console.error('Promise : ', p)
+  })
 }
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug')
-app.use(logger('dev'))
-app.use(helmet())
-app.use(compression())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(session(sessionOptions))
-app.use(csurf())
-app.use(express.static('dist'))
-router(app)
+process.on('unhandledRejection', (err, p) => {
+  logger.error(err, p);
+})
 
-// if (process.env.NODE_ENV !== 'production') {
-//   const webpack = require('webpack');
-//   const webpackHotMiddleware = require('webpack-hot-middleware');
-//   const webpackDevMiddleware = require('webpack-dev-middleware');
-//   const config = require('./webpack.config');
-//   const compiler = webpack(config);
-
-//   app.use(webpackHotMiddleware(compiler));
-//   app.use(
-//     webpackDevMiddleware(compiler, {
-//       publicPath: config.output.publicPath
-//     })
-//   );
-// }
-
-const server = createServer(app)
-server.listen(port);
-
-server.on('listening', () => {
-  const addr = server.address();
-  const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`
-
-  console.log(`Listening on ${bind}`)
-});
-
-server.on('error', (err) => {
-  if (err.syscall !== 'listen') throw err
-
-  const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`
-
-  switch (err.code) {
-    case 'EACCES':
-      console.error(`${bind} requires elevated privileges`);
-      process.exit(1);
-    case 'EADDRINUSE':
-      console.error(`${bind} is already in use`);
-      process.exit(1);
-    default:
-      throw err;
-  }
+process.on('uncaughtException', (err) => {
+  console.error(err)
+  process.exit(1)
 })
---
+++ server/server.js
@@ -0,0 +1,66 @@
+'use strict'
+
+module.exports = () => {
+  const path = require('path')
+  const { createServer } = require('http')
+  const express = require('express')
+  const bodyParser = require('body-parser')
+  const helmet = require('helmet')
+  const compression = require('compression')
+  const session = require('express-session')
+  const csurf = require('csurf')
+  const logger = require('morgan')
+  const router = require('./router')
+
+  const port = process.env.PORT || 3000
+  const app = express()
+  const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      //secure: true,
+      httpOnly: true,
+      domain: process.env.HOST,
+    }
+  }
+
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'pug')
+  app.use(logger('dev'))
+  app.use(helmet())
+  app.use(compression())
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(session(sessionOptions))
+  app.use(csurf())
+  app.use(express.static('dist'))
+  router(app)
+
+  const server = createServer(app)
+  server.listen(port);
+
+  server.on('listening', () => {
+    const addr = server.address();
+    const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`
+
+    console.log(`Listening on ${bind}`)
+  });
+
+  server.on('error', (err) => {
+    if (err.syscall !== 'listen') throw err
+
+    const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`
+
+    switch (err.code) {
+      case 'EACCES':
+        console.error(`${bind} requires elevated privileges`);
+        process.exit(1);
+      case 'EADDRINUSE':
+        console.error(`${bind} is already in use`);
+        process.exit(1);
+      default:
+        throw err;
+    }
+  })
+}
